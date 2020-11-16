e494b91a4fb699a6d9c305c0edad2dd0b2e70eb0
Guillermo Galindo <31331135+DryMemoG@users.noreply.github.com>
2020-11-16 01:06:11 -0600

    Se crearon los Modelos

--- registro/asistencia/models.py
+++ registro/asistencia/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Grado(models.Model):
+    nombre= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+class Actividad(models.Model):
+    nombre=models.CharField(max_length=100)
+    fecha=models.DateField()
+    
+    def __str__(self):
+        return self.nombre
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos= models.CharField(max_length=100)
+    grado = models.ForeignKey(Grado, related_name="Grado", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+        
+class Asistencia(models.Model):
+    nombre=models.CharField(max_length=100)
+    alumno = models.ForeignKey(Estudiante, related_name="Estudiante", on_delete=models.CASCADE)
+    actividad = models.ForeignKey(Actividad, related_name="Actividad", on_delete=models.CASCADE)
+    reporte = models.CharField(max_length=2000)
+    
+    def __str__(self):
+        return self.nombre
